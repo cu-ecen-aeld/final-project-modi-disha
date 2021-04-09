4f18e3302497c4005e5dbc072f07782e2f7402a3
modi-disha <disha.modi@colorado.edu>
2021-04-09 10:27:31 -0600

    socket server client is working

---
+++ Client/SocketClient.c
@@ -0,0 +1,103 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include<syslog.h>
+#include <signal.h>
+#include <stdint.h>
+
+#define MAXRECVSTRING 30
+#define PORTNO 9000
+#define PORT "9000"
+#define pFILE "/var/tmp/sensordata.txt"
+
+int operation_switch =1;
+
+//TCP socket descriptor
+int socket_client;
+
+//Signal HAndler function
+void handle_sig(int sig)
+{
+  operation_switch=0;
+  if(sig == SIGINT)
+    syslog(LOG_DEBUG,"Caught SIGINT Signal exiting\n");
+  if(sig == SIGTERM)
+    syslog(LOG_DEBUG,"Caught SIGTERM Signal exiting\n");  
+  shutdown(socket_client,SHUT_RDWR);
+  _exit(0);
+}
+  
+int main(int argc, char *argv[])
+{
+  signal(SIGTERM,handle_sig);
+  signal(SIGINT,handle_sig);
+
+
+  struct sockaddr_storage opp_addr;
+  struct addrinfo ref, *res, *p;
+  memset(&ref, 0, sizeof(ref));
+  ref.ai_family = AF_UNSPEC;
+  ref.ai_socktype = SOCK_STREAM;
+  ref.ai_flags = AI_PASSIVE;
+
+  if(getaddrinfo(NULL, PORT, &ref, &res) != 0)
+  {
+    syslog(LOG_ERR, "getaddrinfo failed.");
+    return -1;
+  }
+
+  if((socket_client = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0)
+  {
+		perror("socket failed");
+  }
+
+  if(connect(socket_client, res->ai_addr, res->ai_addrlen) == -1)
+  {
+    printf("Connect to server failed\n");
+  }
+  
+
+  while(operation_switch)
+  {
+    char rcv_cmd[20]={0}; 
+    
+    //receive the string
+	ssize_t length = recv(socket_client, rcv_cmd, sizeof(rcv_cmd), 0);
+	
+	rcv_cmd[length] = '\0';
+	//check if read failed
+	if(length == -1) 
+	{
+		perror("recv");
+		printf("recv failed");
+	}
+    
+	//open file for write
+	int fd = open(pFILE, O_RDONLY | O_WRONLY | O_CREAT | O_APPEND, 0644); 
+	if(fd < 0)
+	{
+		printf("File open failed");
+	}
+
+	ssize_t rc = write (fd, (void *)rcv_cmd, strlen(rcv_cmd));
+    
+    if( rc < 0){
+      perror("Couldnt write sensor results to file\n");
+    }
+
+ }
+}
+  //shutdown(socket_client,SHUT_RDWR);
+  
---
+++ Client/SocketServer.c
@@ -0,0 +1,146 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include<syslog.h>
+#include <signal.h>
+#include <stdint.h>
+
+#define MAXRECVSTRING 30
+#define PORTNO 9000
+#define PORT "9000"
+
+#define BACKLOG 10
+int sockfd, newfd;
+char s[INET6_ADDRSTRLEN];
+int operation_switch =1;
+
+//Signal HAndler function
+void handle_sig(int sig)
+{
+  operation_switch=0;
+  if(sig == SIGINT)
+    syslog(LOG_DEBUG,"Caught SIGINT Signal exiting\n");
+  if(sig == SIGTERM)
+    syslog(LOG_DEBUG,"Caught SIGTERM Signal exiting\n");  
+  shutdown(newfd,SHUT_RDWR);
+  _exit(0);
+}
+
+// This routine returns client socket address
+//ref:https://beej.us/guide/bgnet/html/#cb20-1
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {                   
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);   // for IPv6
+}
+  
+int main(int argc, char *argv[])
+{
+  signal(SIGTERM,handle_sig);
+  signal(SIGINT,handle_sig);
+
+  struct sockaddr_storage opp_addr;
+  struct addrinfo ref, *res, *p;
+  memset(&ref, 0, sizeof(ref));
+  ref.ai_family = AF_UNSPEC;
+  ref.ai_socktype = SOCK_STREAM;
+  ref.ai_flags = AI_PASSIVE;
+
+  if(getaddrinfo(NULL, PORT, &ref, &res) != 0)
+  {
+    syslog(LOG_ERR, "getaddrinfo failed.");
+    return -1;
+  }
+
+	  /// socket and bind
+	for(p = res; p != NULL; p = p->ai_next)
+	{
+	
+	  if((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0)
+	  {
+			perror("Socket failed");
+	  }
+	
+	  if(bind(sockfd, res->ai_addr, res->ai_addrlen) < 0)
+	  {
+			perror("bind failed");
+	  }
+	
+	  break;
+	}
+	
+	if(p == NULL)
+	{
+	  syslog(LOG_ERR, "Socket bind failed.");
+		return -1;
+	}
+	
+	freeaddrinfo(res);
+
+//int opt;
+//
+///// start daemon if -d given in argument
+//if((opt = getopt(argc, argv, "d")) == 'd')
+//{
+//	daemon(0, sockfd);
+//}
+
+
+	/// socket listen
+	if(listen(sockfd, BACKLOG) < 0)
+	{
+	  syslog(LOG_ERR, "Socket listen failed.");
+	  return -1;
+	}
+  
+
+  while(operation_switch)
+  {
+		socklen_t addr_size = sizeof(opp_addr);
+		
+		/// accept socket connection
+	newfd = accept(sockfd, (struct sockaddr *)&opp_addr, &addr_size);
+	if(newfd < 0)
+	{
+			syslog(LOG_ERR, "Socket accept failed.");
+		if(!operation_switch) 
+		{
+			break;
+		}
+         exit (EXIT_FAILURE);
+	}
+		
+	inet_ntop(opp_addr.ss_family, get_in_addr((struct sockaddr *)&opp_addr),
+                  s, sizeof s);
+
+	/// LOG MSG TO SYSLOG: "Accepted connection from XXX"
+	syslog(LOG_INFO, "Accepted connection from %s\n", s);
+	
+	char rdBuff[20] = "a = 30";
+	
+	int rc = send(newfd, rdBuff, strlen(rdBuff), MSG_DONTWAIT);
+
+    if( rc < 0){
+      perror("Couldnt send sensor results to file\n");
+    }
+
+    usleep(500);
+ }
+}
+  //shutdown(socket_client,SHUT_RDWR);
+  

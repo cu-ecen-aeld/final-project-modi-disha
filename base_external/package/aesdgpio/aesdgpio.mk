c9926b30dfdd3db38b50389b6c4f338810fd60a6
modi-disha <disha.modi@colorado.edu>
2021-04-12 17:27:21 -0600

    makefile updated

--- base_external/package/aesdgpio/aesdgpio.mk
+++ base_external/package/aesdgpio/aesdgpio.mk
@@ -1,23 +1,23 @@
 
 ##############################################################
 #
-# SOCKETCOMM
+# AESDGPIO
 #
 ##############################################################
 
-SOCKETCOMM_VERSION=a564a66cf60c4daa3bec2d96928031699aeec040
+AESDGPIO_VERSION=5f8290b94c2a674c4a2d28425f9f2eb794b7a87b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-SOCKETCOMM_SITE=git@github.com:cu-ecen-5013/final-project-modi-disha.git
-SOCKETCOMM_SITE_METHOD = git
+AESDGPIO_SITE=git@github.com:cu-ecen-5013/final-project-modi-disha.git
+AESDGPIO_SITE_METHOD = git
 
-define SOCKETCOMM_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/Client all
+define AESDGPIO_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesdgpio all
 endef
 
-define SOCKETCOMM_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/Client/* $(TARGET_DIR)/usr/bin
+define AESDGPIO_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesdgpio/* $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))

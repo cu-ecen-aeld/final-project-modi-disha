20f56bb75684942281111b0860be3b5458b0aae8
modi-disha <disha.modi@colorado.edu>
2021-04-24 15:51:43 -0600

    lcd code updated

--- Client/q_msg_lcd.c
+++ Client/q_msg_lcd.c
@@ -7,7 +7,6 @@
 #include <sys/stat.h>
 #include <mqueue.h>
 #include <wiringPi.h>
-#include <lcd.h>
 
 #define LCD_E 23
 #define LCD_RS 22
@@ -15,12 +14,6 @@
 #define LCD_D5 25
 #define LCD_D6 8
 #define LCD_D7 7
-
-#define SERVER_QUEUE_NAME   "/aesd-server"
-#define QUEUE_PERMISSIONS 0660
-#define MAX_MESSAGES 25
-#define MAX_MSG_SIZE 256
-#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
 
 #define IN  0
 #define OUT 1
@@ -213,7 +206,7 @@ void lcd_text(char *s)
 {
   while(*s) 
 	lcd_byte(*s++);
- }
+}
 
 void lcd_init()
 {
@@ -240,7 +233,7 @@ void lcd_init()
 int lcd_print(char *msg) 
 {
   lcd_byte(0x01);  //Clear screen
-  usleep(1000);        // clear screen is slow!
+  sleep(2);        // clear screen is slow!
   SetChrMode(); 
   lcd_text(msg);
   return 0 ;
@@ -249,18 +242,31 @@ int lcd_print(char *msg)
 int main (int argc, char **argv)
 {
 
-    	int lcd;
-	wiringPiSetup();
-	lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+    printf ("Server: Hello, World!\n");
 
-	lcdPuts(lcd, "Hello, world!");
+    char in_buffer [MSG_BUFFER_SIZE];
+    char *out_buffer ;
 
-	sleep(2);
+   // GPIOinit();
+    lcd_init();
 
-	lcdClear(lcd);
+    while (1) 
+    {
+        // get the oldest message with highest priority
+ 
 
-	lcdPuts(lcd, "Hi Girls, how are you?");
-	sleep(2);
+        printf ("Server: message received.\n");
 
-	lcdClear(lcd);
+	lcd_print("Hello Maitreyee!");
+	printf ("Hello Maitreyee!");
+
+	lcd_print("Hello Swati!");
+	printf ("Hello Swati!");
+	
+	lcd_print("Hello Girls!");
+	printf ("Hello Girls!");
+
+        printf ("Server: response printed on lcd.\n");
+        token_number++;
+    }
 }
--- base_external/package/aesdlcd/aesdlcd.mk
+++ base_external/package/aesdlcd/aesdlcd.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDLCD_VERSION=0b9a669e1726dfda69bb0a719dae5e0a15c4d04b
+AESDLCD_VERSION=3294b3768a5993ab3a9f0ba384d0e72d75b976bf
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:

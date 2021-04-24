3294b3768a5993ab3a9f0ba384d0e72d75b976bf
modi-disha <disha.modi@colorado.edu>
2021-04-24 15:07:42 -0600

    lcd code updated

--- aesdlcd/lcd.c
+++ aesdlcd/lcd.c
@@ -41,15 +41,17 @@ int main (int argc, char **argv)
     	int lcd;
 	wiringPiSetup();
 	lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
-
-	lcdPuts(lcd, "Hello, world!");
+	
+	lcdPosition(lcd, 0, 0);
+	lcdPrintf(lcd, "Hello, world!");
+	//lcdPuts(lcd, "Hello, world!");
 
 	sleep(2);
 	sleep(20);
 
 	lcdClear(lcd);
-
-	lcdPuts(lcd, "Hi Girls, how are you?");
+	lcdPosition(lcd, 0, 0);
+	lcdPrintf(lcd, "Hi Girls, how are you?");
 	sleep(2);
 	
 	//lcdClear(lcd);
--- base_external/package/socketcomm/socketcomm.mk
+++ base_external/package/socketcomm/socketcomm.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-SOCKETCOMM_VERSION=aee392b80df0cf9ecf07abb510bb156f27251067
+SOCKETCOMM_VERSION=c0026732c48ddc6b8ae094fe9acc2f6462fffb56
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:

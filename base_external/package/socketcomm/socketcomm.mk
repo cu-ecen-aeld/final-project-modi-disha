88dd8ef14e9a9a2162deeac7953b844131b972d9
modi-disha <disha.modi@colorado.edu>
2021-04-25 14:34:30 -0600

    gpio sensor data updated

--- base_external/package/socketcomm/socketcomm.mk
+++ base_external/package/socketcomm/socketcomm.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-SOCKETCOMM_VERSION=dcfcbf8bd3b16c15980df9aefa8474e8488d2282
+SOCKETCOMM_VERSION=d241e9d78c3d62268f605a7281afbe70d6403817
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
